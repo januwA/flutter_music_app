3ae3e7d527661119830232ccc3c0253e7634db7d
januwA <ajanuw1641845087@sina.com>
2019-05-13 23:00:44 +0800

    ok,加了个底部的信息框

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/animation.dart';
 import 'package:flute_music_player/flute_music_player.dart';
 
 void main() => runApp(MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
   List<Song> _songs = <Song>[]; // 本地音乐列表
   PlayerState playerState; // 播放状态
   Song playingSong; // 正在播放的音乐
@@ -20,6 +20,7 @@ class _MyAppState extends State<MyApp> {
   // 下一首歌
   Song get nextSong {
     currentSongIndex++;
+    playingSong = _songs[currentSongIndex];
     Song ns = _songs[currentSongIndex % _songs.length];
     return ns;
   }
@@ -27,13 +28,32 @@ class _MyAppState extends State<MyApp> {
   bool _isLoading = true; // 是否在加载(songs)状态
   Duration duration;
   Duration position;
+  Animation<Offset> bottomViewAnimation;
+  AnimationController bottomViewAnimationCtrl;
 
   @override
   void initState() {
     super.initState();
     _initPlayer();
+    _initBottomViewAnimation();
   }
 
+  @override
+  void dispose() {
+    bottomViewAnimationCtrl.dispose();
+    super.dispose();
+  }
+
+  @override
+  void reassemble() {
+    _initPlayer();
+    _initBottomViewAnimation();
+    super.reassemble();
+  }
+
+  /**
+   * * 初始化获取用户本地的音乐列表
+   */
   void _initPlayer() async {
     _isLoading = true;
     var songs = await MusicFinder.allSongs();
@@ -60,6 +80,22 @@ class _MyAppState extends State<MyApp> {
         position = duration;
       });
     });
+  }
+
+  /**
+   * * 初始化底部悬浮view的动画
+   */
+  void _initBottomViewAnimation() {
+    bottomViewAnimationCtrl = new AnimationController(
+      duration: const Duration(
+        milliseconds: 600,
+      ),
+      vsync: this,
+    );
+    bottomViewAnimation = Tween<Offset>(
+      begin: Offset(0, 0),
+      end: Offset(0, 1), // y轴偏移量+height
+    ).animate(bottomViewAnimationCtrl);
   }
 
   // 播放
@@ -114,63 +150,127 @@ class _MyAppState extends State<MyApp> {
                     style: TextStyle(fontSize: 24),
                   ),
                 )
-              : ListView.builder(
-                  itemCount: _songs.length,
-                  itemBuilder: (context, int index) {
-                    Song tapSong = _songs[index];
-                    return new ListTile(
-                      leading: CircleAvatar(
-                        // 没有专辑图片就用文字代替
-                        child: tapSong.albumArt == null
-                            ? Text(tapSong.title[0])
-                            : new Image.file(
-                                new File.fromUri(
-                                  Uri.parse(tapSong.albumArt),
-                                ),
-                                fit: BoxFit.cover,
-                                filterQuality: FilterQuality.low,
-                              ),
-                      ),
-                      title: Text(tapSong.title),
-                      onTap: () async {
-                        currentSongIndex = index;
-
-                        // print(s.id); // 23117
-                        // print(s.album); // ジョジョの奇妙な冒険 O.S.T Battle Tendency [Leicht Verwendbar]
-                        // print(s.albumArt); // /storage/emulated/0/Android/data/com.android.providers.media/albumthumbs/1556812126330
-                        // print(s.albumId); // 7
-                        // print(s.artist);//岩崎琢
-                        // print(s.duration);//201638
-                        // print(s.title);//Awake
-                        // print(s.uri);// /storage/emulated/0/netease/cloudmusic/Music/岩崎琢 - Awake.mp3
-
-                        if (playerState == PlayerState.playing) {
-                          // 暂停
-                          print('暂停');
-                          // 在列表上点击你应该使用"stop()"而不是"pause()",因为stop会让song真正的结束。
-                          if (tapSong == playingSong) {
-                            await pause();
-                          } else {
-                            _switchMusic(tapSong);
-                          }
-                        } else {
-                          // 播放
-                          print('播放');
-                          if (playingSong == null) {
-                            // 第一次进入直接播放点击歌曲
-                            playingSong = tapSong;
-                            await playLocal(playingSong.uri);
-                          } else if (tapSong != playingSong) {
-                            // 在列表中点击了其他歌曲
-                            _switchMusic(tapSong);
-                          } else if (tapSong == playingSong) {
-                            // 点击了同一首歌曲
-                            await playLocal(playingSong.uri);
+              : Stack(
+                  children: <Widget>[
+                    NotificationListener(
+                      onNotification: (notification) {
+                        if (notification is ScrollUpdateNotification &&
+                            notification.depth == 0) {
+                          var d = notification.dragDetails;
+                          if (d != null && d.delta != null) {
+                            int yoff = 3;
+                            var dy = d.delta.dy;
+                            if (dy > yoff) {
+                              // 向下滑动
+                              bottomViewAnimationCtrl.reverse();
+                            } else if (dy < -yoff) {
+                              // 向上滑动
+                              bottomViewAnimationCtrl.forward();
+                            }
                           }
                         }
                       },
-                    );
-                  },
+                      child: ListView.builder(
+                        itemCount: _songs.length,
+                        itemBuilder: (context, int index) {
+                          Song tapSong = _songs[index];
+                          return new ListTile(
+                            leading: fileImage(
+                              tapSong.albumArt == null
+                                  ? Text(tapSong.title[0])
+                                  : tapSong.albumArt,
+                            ),
+                            title: Text(tapSong.title),
+                            onTap: () async {
+                              currentSongIndex = index;
+
+                              // print(s.id); // 23117
+                              // print(s.album); // ジョジョの奇妙な冒険 O.S.T Battle Tendency [Leicht Verwendbar]
+                              // print(s.albumArt); // /storage/emulated/0/Android/data/com.android.providers.media/albumthumbs/1556812126330
+                              // print(s.albumId); // 7
+                              // print(s.artist);//岩崎琢
+                              // print(s.duration);//201638
+                              // print(s.title);//Awake
+                              // print(s.uri);// /storage/emulated/0/netease/cloudmusic/Music/岩崎琢 - Awake.mp3
+
+                              if (playerState == PlayerState.playing) {
+                                // 暂停
+                                print('暂停');
+                                // 在列表上点击你应该使用"stop()"而不是"pause()",因为stop会让song真正的结束。
+                                if (tapSong == playingSong) {
+                                  await pause();
+                                } else {
+                                  _switchMusic(tapSong);
+                                }
+                              } else {
+                                // 播放
+                                print('播放');
+                                if (playingSong == null) {
+                                  // 第一次进入直接播放点击歌曲
+                                  playingSong = tapSong;
+                                  await playLocal(playingSong.uri);
+                                } else if (tapSong != playingSong) {
+                                  // 在列表中点击了其他歌曲
+                                  _switchMusic(tapSong);
+                                } else if (tapSong == playingSong) {
+                                  // 点击了同一首歌曲
+                                  await playLocal(playingSong.uri);
+                                }
+                              }
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      left: 0,
+                      child: playingSong != null
+                          ? SlideTransition(
+                              position: bottomViewAnimation,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                ),
+                                child: ListTile(
+                                  dense: true, // 稍微小点
+                                  leading: fileImage(
+                                    playingSong.albumArt == null
+                                        ? Text(playingSong.title[0])
+                                        : playingSong.albumArt,
+                                  ),
+                                  title: Text(
+                                    playingSong.title,
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 1,
+                                  ),
+                                  subtitle: Text(
+                                    playingSong.album,
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 1,
+                                  ),
+                                  trailing: IconButton(
+                                    icon: Icon(
+                                      playerState != PlayerState.playing
+                                          ? Icons.play_arrow
+                                          : Icons.pause,
+                                      color: Theme.of(context).primaryColor,
+                                    ),
+                                    onPressed: () async {
+                                      if (playerState == PlayerState.playing) {
+                                        await pause();
+                                      } else {
+                                        await playLocal(playingSong.uri);
+                                      }
+                                    },
+                                  ),
+                                ),
+                              ),
+                            )
+                          : Container(),
+                    ),
+                  ],
                 ),
         );
       } else {
@@ -182,14 +282,64 @@ class _MyAppState extends State<MyApp> {
       }
     }
 
+    var show = true;
     return new MaterialApp(
       home: home(),
+      // home: Scaffold(
+      //   body: ListView(
+      //     children: <Widget>[
+      //       FlatButton(
+      //         onPressed: () {
+      //           show = !show;
+      //           if (show) {
+      //             controller.reverse();
+      //           } else {
+      //             controller.forward();
+      //           }
+      //         },
+      //         child: Text('click'),
+      //       ),
+      //       SlideTransition(
+      //         position: Tween<Offset>(
+      //           begin: Offset(0, 1),
+      //           end: Offset(0, 2),
+      //         ).animate(controller),
+      //         child: Container(
+      //           height: 200,
+      //           decoration: BoxDecoration(
+      //             color: Colors.green,
+      //           ),
+      //         ),
+      //       ),
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
 
+// 音乐播放状态
 enum PlayerState {
   playing,
   paused,
   stopped,
+}
+
+// 返回一个本地的image
+fileImage(
+  path, {
+  BoxFit fit = BoxFit.cover,
+  FilterQuality filterQuality: FilterQuality.low,
+}) {
+  return CircleAvatar(
+    child: path is Widget
+        ? path
+        : new Image.file(
+            new File.fromUri(
+              Uri.parse(path),
+            ),
+            fit: fit,
+            filterQuality: filterQuality,
+          ),
+  );
 }
